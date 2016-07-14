e3488f83e29f8932d65c4b7d8ca3b380ff71db69
kate <kate@syoya.com>
2016-07-14 15:41:29 +0900

    Add test to option

--- option/options_test.go
+++ option/options_test.go
@@ -2,6 +2,8 @@ package option_test
 
 import (
 	"github.com/go-microservices/resizer/option"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -50,5 +52,40 @@ func TestFlags(t *testing.T) {
 	}
 	if o.Hosts[1] != "JJJJ" {
 		t.Error("wrong Hosts[1]:", o.Hosts[0])
+	}
+}
+
+func TestEnvar(t *testing.T) {
+	o, err := option.New([]string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if o.ProjectID != os.Getenv("RESIZER_PROJECT_ID") {
+		t.Error("wrong ProjectID:", o.ProjectID)
+	}
+	if o.Bucket != os.Getenv("RESIZER_BUCKET") {
+		t.Error("wrong Bucket:", o.Bucket)
+	}
+	if o.JSON != os.Getenv("RESIZER_JSON") {
+		t.Error("wrong JSON:", o.JSON)
+	}
+	if o.DBUser != os.Getenv("RESIZER_DB_USER") {
+		t.Error("wrong DBUser:", o.DBUser)
+	}
+	if o.DBPassword != os.Getenv("RESIZER_DB_PASSWORD") {
+		t.Error("wrong DBPassword:", o.DBPassword)
+	}
+	if o.DBProtocol != os.Getenv("RESIZER_DB_PROTOCOL") {
+		t.Error("wrong DBProtocol:", o.DBProtocol)
+	}
+	if o.DBAddress != os.Getenv("RESIZER_DB_ADDRESS") {
+		t.Error("wrong DBAddress:", o.DBAddress)
+	}
+	if o.DBName != os.Getenv("RESIZER_DB_NAME") {
+		t.Error("wrong DBName:", o.DBName)
+	}
+	SplitedHosts := strings.Split(os.Getenv("RESIZER_HOSTS"), ",")
+	if o.Hosts[0] != SplitedHosts[0] {
+		t.Error("wrong Hosts:", o.Hosts, "expect:", SplitedHosts)
 	}
 }
